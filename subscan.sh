eb08d68a384b23fc6405e85bd9719f79c273b132
rius-admin <158822649+rius-admin@users.noreply.github.com>
2025-02-22 18:06:51 +0700

    Update subscan.sh

--- subscan.sh
+++ subscan.sh
@@ -5,17 +5,14 @@
 
 touch tmp.txt
 echo " "
-echo " #====================#"
-echo " #   SUBDOMAIN SCAN   #"
-echo " #====================#"
+echo " #========================#"
+echo " #     SUBDOMAIN SCAN     #"
+echo " #========================#"
 echo " "
 echo " "
-echo " progres : ./subscan.sh target.com "
+echo " [•] loading.."
 echo " "
-echo " [•] loading.. 
 echo " " 
-echo " "
-echo "
 curl --silent --insecure --tcp-fastopen --tcp-nodelay "https://rapiddns.io/subdomain/$1?full=1#result" | grep "<td><a" | cut -d '"' -f 2 | grep http | cut -d '/' -f3 | sed 's/#results//g' | sort -u >> tmp.txt &
 curl --silent --insecure --tcp-fastopen --tcp-nodelay "http://web.archive.org/cdx/search/cdx?url=*.$1/*&output=text&fl=original&collapse=urlkey" | sed -e 's_https*://__' -e "s/\/.*//" | sort -u >> tmp.txt &
 curl --silent --insecure --tcp-fastopen --tcp-nodelay https://crt.sh/?q=%.$1  | grep -oP "\<TD\>\K.*\.$1" | sed -e 's/\<BR\>/\n/g' | grep -oP "\K.*\.$1" | sed -e 's/[\<|\>]//g' | grep -o -E "[a-zA-Z0-9._-]+\.$1"  >> tmp.txt &
